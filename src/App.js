8052393406cfcdb4cabc6e3467d30df858490ffc
Whoisbezo.fx <bezzo19@gmx.de>
2025-11-29 22:32:50 +0100

    Add background div to App component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    <div className="background">
+      {/* Ihr Content hier */}
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
