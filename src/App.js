ff725beb5c4cf254bf4aa50e68e9d36ce57d6e87
Whoisbezo.fx <bezzo19@gmx.de>
2025-11-30 23:11:36 +0100

    App.js aktualisieren

--- src/App.js
+++ src/App.js
@@ -3,40 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        {/* Topbar */}
-      <div className="topbar">
-        <div className="topbar-content">
-          {/* Links: Logo */}
-          <div className="topbar-left">
-            <div className="logo">RendexFi</div>
-          </div>
-
-          {/* Mitte: Suchleiste */}
-          <div className="topbar-center">
-            <div className="search-bar">
-              <input 
-                type="text" 
-                placeholder="Tokens, Pools suchen..."
-                className="search-input"
-              />
-              <button className="search-button">
-                🔍
-              </button>
-            </div>
-          </div>
-
-          {/* Rechts: Settings + Connect Wallet */}
-          <div className="topbar-right">
-            <button className="settings-button">
-              ⚙️
-            </button>
-            <button className="connect-wallet-button">
-              Connect Wallet
-            </button>
-          </div>
-        </div>
+    {/* Leere Navbar */}
+    <nav className="navbar">
+      <div className="nav-wrapper">
+        {/* Keine Inhalte */}
       </div>
-    </div>
+    </nav>
+    <\div>
   );
 }
 
