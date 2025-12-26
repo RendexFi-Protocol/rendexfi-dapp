6e690f3d2afefd6a068f376dab1dd1f1cb133cdd
Whoisbezo.fx <bezzo19@gmx.de>
2025-12-26 16:49:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
+import TopButtons from "./components/chrome/TopButtons";
 
 import './layout.css';
 
 function App() {
   return (
     <div className="App">
+      <TopButtons />
     </div>
   );
 }
