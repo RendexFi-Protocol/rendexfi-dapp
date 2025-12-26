130454877f16bd2c91bfd6036e6b3040df4de184
Whoisbezo.fx <bezzo19@gmx.de>
2025-12-26 17:28:46 +0100

    WalletIcon.jsx erstellen

---
+++ src/components/icons/WalletIcon.jsx
@@ -0,0 +1,17 @@
+export default function WalletIcon({ size = 22 }) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.8"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M21 12V7a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-5" />
+      <path d="M16 12h2" />
+    </svg>
+  );
+}
